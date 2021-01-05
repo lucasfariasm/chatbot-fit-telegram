9df654bd9d71a1fcee2369846f01da9a806e7cc0
Valdir Moura <lucasfarias.bsb@hotmail.com>
2021-01-05 17:08:09 -0300

    atualization

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '1545840889:AAHP8gnv9A3JycnDo21bqXBs55hkciGFHZU';
 
 const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', function(msg) {
+  const chatId = msg.chat.id;
+  console.log(msg.text);
+  bot.sendMessage(chatId, 'Fale aí, meu chegado')
+});
